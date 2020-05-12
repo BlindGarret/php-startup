cfaab9bb35f2ca372facae523504210a94dd12d1
Lucas Roe <lroe2930@gmail.com>
2020-05-12 01:08:16 -0400

    Initial Setup

---
+++ public_html/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Description of what this module (or file) is doing.
+ *
+ * @author   Lucas Roe <lroe2930@gmail.com>
+ */
+?>
+<h1>Hello World!</h1>
+
+
+<?php
+$host = getenv('DATABASE_HOST');
+$user = getenv('DATABASE_USER');
+$pass = getenv('DATABASE_PASS');
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("DB Connection failed: " . $conn->connect_error);
+}
+echo "Connected to MySQL successfully!";
+?>
